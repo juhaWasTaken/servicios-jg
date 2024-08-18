5b61818fbee3bd5b35feae9f8618c329731f3e8d
Carlos Gallaga <carlos.gallagav@gmail.com>
2024-08-17 19:02:55 -0500

    New Push?

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			blueMain: 'var(--blueMain)',
+			blueLight: 'var(--blueLight)',
+			blueDarker: 'var(--blueDarker)',
+			blueLighter: 'var(--blueLighter)',
+			white: 'var(--white)',
+			black: 'var(--black)',
+		}
+	},
+	plugins: [],
+}
